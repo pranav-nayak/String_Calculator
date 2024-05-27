f5efbba99cbbf0db010562f9f8ff9c48b5309da8
pranav-nayak <prasannabr199595@gmail.com>
2024-05-27 13:11:22 +0530

    For single and two input numbers

--- app/models/strings_calculator.rb
+++ app/models/strings_calculator.rb
@@ -1,5 +1,7 @@
 class StringsCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    return numbers.to_i unless numbers.include?(",")
+    numbers.split(",")[0].to_i + numbers.split(",")[1].to_i
   end
 end
--- spec/models/strings_calculator_spec.rb
+++ spec/models/strings_calculator_spec.rb
@@ -5,5 +5,25 @@ RSpec.describe StringsCalculator, type: :model do
     it 'returns 0 for an empty string' do
       expect(StringsCalculator.new.add("")).to eq(0)
     end
+
+    context "Single number" do
+      it 'returns number 1 for "1"' do
+        expect(StringsCalculator.new.add("1")).to eq(1)
+      end
+
+      it 'returns number 38 for "38"' do
+        expect(StringsCalculator.new.add("38")).to eq(38)
+      end
+    end
+
+    context "Two numbers" do
+      it 'returns number 6 for "1,5"' do
+        expect(StringsCalculator.new.add("1,5")).to eq(6)
+      end
+
+      it 'returns number 25 for "5,20"' do
+        expect(StringsCalculator.new.add("5,20")).to eq(25)
+      end
+    end
   end
 end
